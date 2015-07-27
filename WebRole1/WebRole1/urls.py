72b6162772b759aa15a54804b8f6e4a6c2131184
tmullaney <tmullaney33@gmail.com>
2015-07-27 14:43:02 -0700

    initial commit

---
+++ WebRole1/WebRole1/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for WebRole1.
+"""
+
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'WebRole1.views.home', name='home'),
+    # url(r'^WebRole1/', include('WebRole1.WebRole1.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
